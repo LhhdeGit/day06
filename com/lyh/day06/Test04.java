a554f2ee6602b437250ac9be19729d5ca553cc4d
li <L1832934409@163.com>
2021-07-13 23:36:33 +0800

    1

---
+++ com/lyh/day06/Test04.java
@@ -0,0 +1,17 @@
+package com.lyh.day06;
+
+import java.util.Scanner;
+
+public class Test04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入一个email地址");
+        String email = scanner.nextLine();
+        if(email.indexOf("@")<1){
+            System.out.println("缺少@或没有用户名");
+        }else{
+            String username = email.substring(0, email.indexOf("@"));
+            System.out.println("用户名:"+username);
+        }
+    }
+}
